355753880ccbc5242a73b7d0feb001e955cef5ec
ksrinreddy <ksrinreddy@gmail.com>
2023-05-19 14:52:02 +0530

    built app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 
 const App = () => {
-  return <div>App</div>;
+  const [people, setPeople] = useState(data);
+  return (
+    <>
+      <main>
+        <section className="container">
+          <h3>{people.length} birthdays today</h3>
+          <List people={people} />
+          <button className="btn" onClick={() => setPeople([])}>
+            clear all
+          </button>
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default App;
